86d039d960fb3df87b89051247a11036020d3f29
Carina0229 <caoruiyan7269@163.com>
2022-02-13 14:57:46 +0800

    feat:100.相同的树

---
+++ 树/100.相同的树/100.相同的树.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+  if (!p && !q) {
+      return true;
+  } else if (p?.val !== q?.val) {
+      return false;
+  } else {
+      if ((p.left && q.left && isSameTree(p.left, q.left) || (!p.left && !q.left)) && (q.right &&p.right && isSameTree(p.right, q.right)  || (!p.right && !q.right))) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+};
